6664c9b135e4c16c119e9551861c667ec582d2fa
BSCCO <295872776@qq.com>
2017-03-31 20:34:09 +0800

    Chapter 3, exercise 1: second edition of function "skips"

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,10 +1,7 @@
 module Golf where
 
-pickElem :: Int->[a]->[a]
-pickElem n s = case xs of
-        []->[]
-        x:xss -> x:pickElem n xss
-        where xs = drop n s
+pickElem :: Int->[a]->[(Int,a)]
+pickElem n = filter (\(num,_) -> num `mod` (n+1) == 0) . zip [1..]
 
 skips :: [a] -> [[a]]
-skips x = [ pickElem n x | n <- [0..length x - 1]]
+skips x = [ s | (_,s) <- [unzip (pickElem n x) | n <- [0..length x - 1]]]
