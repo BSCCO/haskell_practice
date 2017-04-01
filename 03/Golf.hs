f12272863c7f81b24b8cdca0b76c062c7f97abac
BSCCO <295872776@qq.com>
2017-04-01 18:29:17 +0800

    Chapter 3, exercise 1: third edition of function "skips"
    The length of third edition is 89 bytes, while second edition is 90 bytes and first edition 70bytes. It should came to mind that recursive function helps a lot to reduce the size!!!

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,7 +1,17 @@
 module Golf where
 
-pickElem :: Int->[a]->[(Int,a)]
-pickElem n = filter (\(num,_) -> num `mod` (n+1) == 0) . zip [1..]
+import           Data.Array
+
+count :: String -> Int
+count = length . filter (\c -> c `notElem` [' ', '\t'])
+
+--short for length
+l :: [a] -> Int
+l = length
+
+--pickup every nth elem in [a] to form a new list
+p :: Int->[a]->[a]
+p n x = [array (1, l x) (zip [1..] x)!m | m<-[1..l x], m `mod` (n+1) == 0]
 
 skips :: [a] -> [[a]]
-skips x = [ s | (_,s) <- [unzip (pickElem n x) | n <- [0..length x - 1]]]
+skips x = [p n x | n <- [0..l x - 1]]
