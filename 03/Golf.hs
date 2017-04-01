d956e3816c790e17e0e7e993e2e154d78da15c52
BSCCO <295872776@qq.com>
2017-04-01 21:32:52 +0800

    Chapter 3, exercise 1: last edition, 55 characters

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,17 +1,13 @@
 module Golf where
 
-import           Data.Array
-
 count :: String -> Int
 count = length . filter (\c -> c `notElem` [' ', '\t'])
 
---short for length
-l :: [a] -> Int
-l = length
-
 --pickup every nth elem in [a] to form a new list
 p :: Int->[a]->[a]
-p n x = [array (1, l x) (zip [1..] x)!m | m<-[1..l x], m `mod` (n+1) == 0]
+p n s = case drop n s of
+        [] -> []
+        x:y -> x:p n y
 
 skips :: [a] -> [[a]]
-skips x = [p n x | n <- [0..l x - 1]]
+skips x = [p n x | n <- [0..length x - 1]]
