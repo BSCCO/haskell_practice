2cdcc27cb544457f681ec7abf2c47d6743ed9814
BSCCO <295872776@qq.com>
2017-04-04 22:13:54 +0800

    Chapter 3, exercise 2: first edition, normal solution.

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,7 +1,9 @@
 module Golf where
 
+import Data.Char
+
 count :: String -> Int
-count = length . filter (\c -> c `notElem` [' ', '\t'])
+count = length . filter (not . isSpace)
 
 --pickup every nth elem in [a] to form a new list
 p :: Int->[a]->[a]
@@ -11,3 +13,11 @@ p n s = case drop n s of
 
 skips :: [a] -> [[a]]
 skips x = [p n x | n <- [0..length x - 1]]
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] =[]
+localMaxima [_] = []
+localMaxima [_, _] = []
+localMaxima (x:y:z:q)
+    | y > x && y > z = y:localMaxima (y:z:q)
+    | otherwise = localMaxima (y:z:q)
