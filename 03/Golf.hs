ee6751617f3773757a092ee55dc6681d2f00baae
BSCCO <295872776@qq.com>
2017-04-05 23:59:28 +0800

    Chapter 3, exercise 2: simplify function name to reduce size, 65 characters.

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,6 +1,6 @@
 module Golf where
 
-import Data.Char
+import           Data.Char
 
 count :: String -> Int
 count = length . filter (not . isSpace)
@@ -8,16 +8,18 @@ count = length . filter (not . isSpace)
 --pickup every nth elem in [a] to form a new list
 p :: Int->[a]->[a]
 p n s = case drop n s of
-        [] -> []
+        []  -> []
         x:y -> x:p n y
 
 skips :: [a] -> [[a]]
 skips x = [p n x | n <- [0..length x - 1]]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima [] =[]
-localMaxima [_] = []
-localMaxima [_, _] = []
-localMaxima (x:y:z:q)
-    | y > x && y > z = y:localMaxima (y:z:q)
-    | otherwise = localMaxima (y:z:q)
+localMaxima = l
+
+l :: [Integer] -> [Integer]
+l s
+    | length s < 3 = []
+l (x:y:z:q)
+    | y > x && y > z = y:l (y:z:q)
+    | otherwise = l $ y:z:q
