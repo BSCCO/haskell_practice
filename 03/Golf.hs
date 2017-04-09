9e894624372f1f57ff87509bc11c4ab71aa5d365
BSCCO <295872776@qq.com>
2017-04-09 22:23:43 +0800

    Chapter 3, exercise 3: first edition that works

--- 03/Golf.hs
+++ 03/Golf.hs
@@ -1,6 +1,7 @@
 module Golf where
 
 import           Data.Char
+import          Data.List
 
 count :: String -> Int
 count = length . filter (not . isSpace)
@@ -23,3 +24,16 @@ l s
 l (x:y:z:q)
     | y > x && y > z = y:l (y:z:q)
     | otherwise = l $ y:z:q
+
+
+--count how many element n is in list a.
+c :: Eq a => a -> [a] -> Int
+c n = length . filter (==n)
+
+-- creat a string with first nth elements are '*' and  total length is m
+t :: Int -> Int -> String
+t n m = take (m-n) (cycle " ") ++ take n (cycle "*")
+
+histogram :: [Integer] -> String
+histogram s = unlines $ transpose [t (c n s) m | n <- [0..9]] ++ ["==========", "0123456789"]
+    where m = maximum [c n s | n <- [0..9]]
